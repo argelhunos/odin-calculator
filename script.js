1bbb83506c4e51542bdfdc15319bc6a9e6aebb79
argelhunos <52228971+argelhunos@users.noreply.github.com>
2024-01-01 19:36:02 -0500

    Add skeleton html, css with starter JS calculator funcs

---
+++ script.js
@@ -0,0 +1,34 @@
+let argument1 = 0;
+let argument2 = 0;
+let operator = "";
+
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+function operate(op, num1, num2) {
+    switch (op) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            break;
+    }
+}
